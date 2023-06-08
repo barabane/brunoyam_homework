46aaee620bd0ed79d76973b81a55357fc35d9d86
barabane <barabayne@gmail.com>
2023-06-08 12:48:14 +0500

    fix

--- 7 module/task.py
+++ 7 module/task.py
@@ -8,17 +8,23 @@ np.random.seed(3)
 x = 0.5 + np.arange(8)
 y = np.random.uniform(2, 7, len(x))
 
-response = request(method='GET', url='https://mfd.ru/currency/?currency=USD')
-
-table = BS(response.text).find('table', class_='mfd-table mfd-currency-table')
-
 usdt_courses = []
+page = request(method='GET', url='https://mfd.ru/currency/?currency=USD')
+soup = BS(page.text, 'html.parser')
+table = soup.find('table',{'class':'mfd-table mfd-currency-table'})
+tds = table.find_all('td')
+tds = (i.text for i in tds)
 
+rows = []
+row = []
 i = 0
-for tr in table:
-    if i < 10:
-        usdt_courses.append(tr.find_all_next('td')[1].text)
-        i += 1
+for td in tds:
+    row.append(td)
+    i += 1
+    if i == 3:
+        usdt_courses.append(row[1])
+        i = 0
+        row.clear()
 
 fig, ax = plt.subplots()  # Create a figure containing a single axes.
 ax.plot(usdt_courses) 
